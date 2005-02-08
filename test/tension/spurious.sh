6e48286b5a6716eb7aae1ff41611266f95109387
Stephane Popinet <s.popinet@niwa.co.nz>
2005-02-08 03:28:23 +0000

    Spurious currents test

---
+++ test/tension/spurious.sh
@@ -0,0 +1,52 @@
+# !/bin/sh
+
+if test -f circle.gts; then
+    :
+else
+    ../poisson/shapes ellipse | transform --scale 0.8 > circle.gts
+fi
+
+param=`mktemp /tmp/spurious.XXXXXX`
+
+for La in 12000 1200 120; do
+    mu=`echo $La | awk '{print sqrt (0.4/$1)}'`
+    tmax=`echo $mu | awk '{print 1000.*$1*0.4}'`
+    du=`echo $mu | awk '{print 1e-7/$1}'`
+    cat <<EOF > $param
+1 2 GfsSimulation GfsBox GfsGEdge {} {
+  Time {
+    end = $tmax
+    dtmax = 1e-3
+  }
+  ApproxProjectionParams { tolerance = 1e-6 }
+  ProjectionParams { tolerance = 1e-6 }
+  Refine 5
+  VariableTracer {} T { scheme = vof }
+  SourceTension {} U V T 1
+  AdvectionParams { scheme = none }
+  SourceDiffusion {} U $mu
+  SourceDiffusion {} V $mu
+  InitFraction {} T circle.gts
+  EventStop { istep = 1 } U $du
+  OutputScalarStats {istep = 1} log-$La { v = P }
+  OutputScalarNorm {istep = 1} log-$La { v = Velocity }
+  OutputScalarSum { istep = 1 } log-$La { v = T }
+}
+GfsBox {}
+1 1 right 
+1 1 top
+EOF
+    gerris2D $param
+    if awk -v La=$La -v mu=$mu '{if ($1 == "Velocity") {max1 = max; max = $9;}} END {
+      printf ("Laplace number: %6g max(U)*mu/sigma: %g\n", La, max1*mu);
+      if (max1*mu > 2.5e-3)
+        exit 1;
+    }' < log-$La; then
+	:
+    else
+	rm -f $param
+	exit 1
+    fi
+done
+
+rm -f $param
