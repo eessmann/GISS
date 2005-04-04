6a41a4d05044de2367834238168600c00ae1463d
Stephane Popinet <s.popinet@niwa.co.nz>
2005-04-04 00:03:56 +0000

    Automatic dependency of examples doc on files generated by examples

--- doc/examples/depend.py
+++ doc/examples/depend.py
@@ -5,12 +5,18 @@ import os
 import os.path
 import gfs2tex
 
-print "EXTRA_DIST +=",
+dists = ""
+depends = ""
 for start in sys.argv[1:]:
     for root, dirs, files in os.walk(start,topdown=True):
         if not ".xvpics" in root:
             example = gfs2tex.Example(root)
-            print example.path + "/" + example.name + ".gfs",
+            dists += "\\\n\t" + example.path + "/" + example.name + ".gfs "
+            depends += "\\\n\t" + example.path + "/" + example.name + ".gfs "
             for f in example.required:
-                print example.path + "/" + f,
+                dists += "\\\n\t" + example.path + "/" + f + " "
+            for f in example.generated:
+                depends += "\\\n\t" + example.path + "/" + f + " "
+print "EXTRA_DIST += " + dists
 print ""
+print "examples.tex: " + depends
