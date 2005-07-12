82d11b7d0f32e756c4bdf9597c572f134286b92f
Stephane Popinet <s.popinet@niwa.co.nz>
2005-07-12 06:50:19 +0000

    Added tarball target for tests docs

--- test/euler/depend.py
+++ test/euler/depend.py
@@ -8,17 +8,22 @@ import gfs2tex
 
 dists = ""
 depends = ""
+docs = ""
 for start in sys.argv[1:]:
     for root, dirs, files in os.walk(start,topdown=True):
         if not ".xvpics" in root:
             test = gfs2tex.Example(root)
-            name = test.path + "/" + test.name + ".gfs "
+            name = test.path + "/" + test.name + ".gfs"
+            docs += "\\\n\t" + name + ".html"
             dists += "\\\n\t" + name
-            depends += "\\\n\t" + name            
+            depends += "\\\n\t" + name
             for f in test.required:
-                dists += "\\\n\t" + test.path + "/" + f + " "
+                dists += "\\\n\t" + test.path + "/" + f
             for f in test.generated:
-                depends += "\\\n\t" + test.path + "/" + f + " "
+                depends += "\\\n\t" + test.path + "/" + f
+
+print "DOCS = " + docs + dists
+print ""
 print "EXTRA_DIST += " + dists
 print ""
 print "tests.tex: " + depends
