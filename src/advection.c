5fdf875540e60e5d3843b1e33a9cb594f8031c32
Stephane Popinet <s.popinet@niwa.co.nz>
2005-10-04 22:09:02 +0000

    Fix for upwinding of MAC velocities
    The "lid-driven cavity" test case was broken by patch "Face boundary
    conditions modified for consistent upwinding". When Dirichlet
    conditions are imposed on the normal velocities, strict upwinding can
    cause a violation of the solvability condition for the Poisson
    equation. This patch fixes this problem by imposing the boundary
    condition but only for normal MAC velocities (in effect reverting to
    the old formulation in this case only).

--- src/advection.c
+++ src/advection.c
@@ -516,7 +516,10 @@ void gfs_face_advected_normal_velocity (const FttCellFace * face,
   if (GFS_FACE_FRACTION (face) == 0.)
     return;
 
-  GFS_FACE_NORMAL_VELOCITY_LEFT (face) = u = gfs_face_upwinded_value (face, par->upwinding, par->u);
+  GFS_FACE_NORMAL_VELOCITY_LEFT (face) = u =
+    GFS_CELL_IS_BOUNDARY (face->neighbor) ?
+    GFS_STATE (face->neighbor)->f[FTT_OPPOSITE_DIRECTION (face->d)].v :
+    gfs_face_upwinded_value (face, par->upwinding, par->u);
 
   switch (ftt_face_type (face)) {
   case FTT_FINE_FINE:
