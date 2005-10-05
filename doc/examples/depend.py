535ddb66f8063cbdca723b91e73481bb25c526bf
Stephane Popinet <s.popinet@niwa.co.nz>
2005-10-05 00:47:24 +0000

    Added examples.tar.gz target

--- doc/examples/depend.py
+++ doc/examples/depend.py
@@ -7,16 +7,22 @@ import gfs2tex
 
 dists = ""
 depends = ""
+docs = ""
 for start in sys.argv[1:]:
     for root, dirs, files in os.walk(start,topdown=True):
         if not ".xvpics" in root:
             example = gfs2tex.Example(root)
-            dists += "\\\n\t" + example.path + "/" + example.name + ".gfs "
-            depends += "\\\n\t" + example.path + "/" + example.name + ".gfs "
+            name = example.path + "/" + example.name + ".gfs"
+            docs += "\\\n\t" + name + ".html"
+            dists += "\\\n\t" + name
+            depends += "\\\n\t" + name
             for f in example.required:
-                dists += "\\\n\t" + example.path + "/" + f + " "
+                dists += "\\\n\t" + example.path + "/" + f
             for f in example.generated:
-                depends += "\\\n\t" + example.path + "/" + f + " "
+                depends += "\\\n\t" + example.path + "/" + f
+
+print "DOCS = " + docs + dists
+print ""
 print "EXTRA_DIST += " + dists
 print ""
 print "examples.tex: " + depends
