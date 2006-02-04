0b4a1d8a73457ba1200d0cbc1d7fd79d6c9c47fb
Stephane Popinet <popinet@users.sf.net>
2006-02-04 13:43:22 +0000

    Updated spurious current test case

--- test/spurious/spurious.sh
+++ test/spurious/spurious.sh
@@ -1,35 +1,33 @@
 if ! $donotrun; then
-    shapes ellipse | transform --scale 0.8 > circle.gts
+    shapes ellipse | transform --scale 1.6 | transform --tx -0.5 --ty 0.5 > circle.gts
     for La in 12000 1200 120; do
-	mu=`echo $La | awk '{print sqrt (0.4/$1)}'`
-	tmax=`echo $mu | awk '{print 1000.*$1*0.4}'`
-	if sed "s/LEVEL/5/g" < $1 |\
+	mu=`echo $La | awk '{print sqrt (0.8/$1)}'`
+	tmax=`echo $mu | awk '{print 1000.*$1*0.8}'`
+	if sed "s/LEVEL/6/g" < $1 |\
            sed "s/MU/$mu/g" |\
-           sed "s/TMAX/$tmax/g" | gerris2D - | awk -v mu=$mu -v D=0.4 '{
-             print $3/(mu*D) " " $9*mu;
-          }' > La\=$La; then :
+           sed "s/TMAX/$tmax/g" | gerris2D - > La\=$La; then :
 	else
 	    exit 1
 	fi
     done
 
     rm -f convergence
-    for level in 5 6 7; do
+    for level in 4 5 6; do
         La=12000
-        mu=`echo $La | awk '{print sqrt (0.4/$1)}'`
-	tmax=`echo $mu | awk '{print 1000.*$1*0.4}'`
+        mu=`echo $La | awk '{print sqrt (0.8/$1)}'`
+	tmax=`echo $mu | awk '{print 1000.*$1*0.8}'`
 	if sed "s/LEVEL/$level/g" < $1 |\
            sed "s/MU/$mu/g" |\
-           sed "s/TMAX/$tmax/g" | gerris2D - | awk -v mu=$mu -v level=$level -v D=0.4 '{ 
-             max = $9
-           }END{print D*2**level " " max*mu}' >> convergence; then :
+           sed "s/TMAX/$tmax/g" | gerris2D - | awk -v level=$level '{
+             max = $2
+           }END{print 0.8*2**level " " max}' >> convergence; then :
 	else
 	    exit 1
 	fi
     done
 fi
 
-if cat <<EOF | gfsview-batch2D sim-5 vectors.gfv ; then :
+if cat <<EOF | gfsview-batch2D sim-4 vectors.gfv ; then :
 Save vectors.eps { format = EPS line_width = 1 }
 EOF
 else
@@ -41,11 +39,13 @@ if cat <<EOF | gnuplot ; then :
     set output 'laplace.eps'
     set xlabel 'Tau'
     set ylabel 'Ca'
+    set logscale y
     plot 'La=120' w l, 'La=1200' w l, 'La=12000' w l
     set output 'convergence.eps'
     set xlabel 'D'
     set ylabel 'Ca'
     unset key
+    unset logscale
     plot [][0:]'convergence' w lp ps 3
 EOF
 else
@@ -55,7 +55,7 @@ fi
 if cat <<EOF | python ; then :
 from check import *
 from sys import *
-if (Curve('convergence',1,2) - Curve('convergence.ref',1,2)).max() > 1e-6:
+if (Curve('convergence',1,2) - Curve('convergence.ref',1,2)).max() > 1e-8:
     exit(1)
 EOF
 else
