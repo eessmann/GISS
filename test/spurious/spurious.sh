a86a359cbc48dc45f3daa7143ff22c6531396a16
Stephane Popinet <popinet@users.sf.net>
2006-10-20 01:39:50 +0000

    Updated spurious currents test case

--- test/spurious/spurious.sh
+++ test/spurious/spurious.sh
@@ -1,52 +1,57 @@
 if ! $donotrun; then
-    shapes ellipse | transform --scale 1.6 | transform --tx -0.5 --ty 0.5 > circle.gts
-    for La in 12000 1200 120; do
+    shapes -n 1000 ellipse | transform --scale 1.6 | transform --tx -0.5 --ty 0.5 > circle.gts
+    for La in 120 1200 12000; do
 	mu=`echo $La | awk '{print sqrt (0.8/$1)}'`
-	tmax=`echo $mu | awk '{print 1000.*$1*0.8}'`
-	if sed "s/LEVEL/6/g" < $1 |\
+	tmax=`echo $mu | awk '{print 0.8*0.8/$1}'`
+	if sed "s/LEVEL/5/g" < $1 |\
            sed "s/MU/$mu/g" |\
-           sed "s/TMAX/$tmax/g" | gerris2D - > La\=$La; then :
+	   sed "s/LAPLACE/$La/g" |\
+	   sed "s/DT/0/" |\
+           sed "s/end = TMAX/iend = 1/g" | gerris2D - |\
+           sed "s/iend = 1/end = $tmax/" | gerris2D - > /dev/null; then :
 	else
 	    exit 1
 	fi
     done
 
     rm -f convergence
-    for level in 4 5 6; do
-        La=12000
+    La=12000
+    for level in 4 6 7; do
         mu=`echo $La | awk '{print sqrt (0.8/$1)}'`
-	tmax=`echo $mu | awk '{print 1000.*$1*0.8}'`
+	tmax=`echo $mu | awk '{print 0.8*0.8/$1}'`
 	if sed "s/LEVEL/$level/g" < $1 |\
            sed "s/MU/$mu/g" |\
-           sed "s/TMAX/$tmax/g" | gerris2D - | awk -v level=$level '{
-             max = $2
-           }END{print 0.8*2**level " " max}' >> convergence; then :
+	   sed "s/LAPLACE/$La/g" |\
+	   sed "s/DT/1e-9/" |\
+           sed "s/end = TMAX/iend = 1/g" | gerris2D - |\
+           sed "s/iend = 1/end = $tmax/" | gerris2D - > sim-$level; then : 
+	else
+	    exit 1
+	fi
+    done
+    for level in 4 5 6 7; do
+	if awk -v level=$level < E-$La-$level '{
+             max2 = $3
+             maxi = $4
+           }END{print 0.8*2**level " " max2 " " maxi}' >> convergence; then : 
 	else
 	    exit 1
 	fi
     done
 fi
 
-if cat <<EOF | gfsview-batch2D sim-4 vectors.gfv ; then :
-Save vectors.eps { format = EPS line_width = 1 }
-EOF
-else
-    exit 1
-fi
-
 if cat <<EOF | gnuplot ; then :
     set term postscript eps color lw 3 solid 20
     set output 'laplace.eps'
     set xlabel 'Tau'
-    set ylabel 'Ca'
+    set ylabel 'U(D/sigma)^1/2'
     set logscale y
-    plot 'La=120' w l, 'La=1200' w l, 'La=12000' w l
+    plot 'La-120-5' w l t "La=120", 'La-1200-5' w l t "La=1200", 'La-12000-5' w l t "La=12000"
     set output 'convergence.eps'
     set xlabel 'D'
-    set ylabel 'Ca'
-    unset key
-    unset logscale
-    plot [][0:]'convergence' w lp ps 3
+    set ylabel 'Shape error'
+    set logscale x
+    plot [10:120]'convergence' u 1:2 w lp t "RMS" ps 3, 'convergence' u 1:3 w lp t "Max" ps 3, 0.2/(x*x) t "Second order"
 EOF
 else
     exit 1
@@ -55,7 +60,7 @@ fi
 if cat <<EOF | python ; then :
 from check import *
 from sys import *
-if (Curve('convergence',1,2) - Curve('convergence.ref',1,2)).max() > 1e-8:
+if (Curve('convergence',1,3) - Curve('convergence.ref',1,3)).max() > 1e-6:
     exit(1)
 EOF
 else
