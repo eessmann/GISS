66506112f655b607d324cbfe670aede7633d8f72
Stephane Popinet <popinet@users.sf.net>
2007-02-21 21:34:27 +0000

    Updated gfs_classes()

--- src/init.c
+++ src/init.c
@@ -101,14 +101,15 @@ GtsObjectClass ** gfs_classes (void)
 
   gfs_variable_class (),
     gfs_variable_tracer_class (),
+      gfs_variable_tracer_vof_class (),
     gfs_variable_residual_class (),
     gfs_variable_filtered_class (),
     gfs_variable_curvature_class (),
-
+      gfs_variable_position_class (),
+    gfs_variable_distance_class (),
   gfs_surface_bc_class (),
 
   gfs_box_class (),
-    gfs_box_not_adapt_class (),
   gfs_gedge_class (),
 
   gfs_bc_dirichlet_class (),
@@ -118,6 +119,7 @@ GtsObjectClass ** gfs_classes (void)
   gfs_boundary_class (),
     gfs_boundary_inflow_constant_class (),
     gfs_boundary_outflow_class (),
+    gfs_boundary_periodic_class (),
 #ifdef HAVE_MPI
     gfs_boundary_mpi_class (),
 #endif /* HAVE_MPI */
@@ -141,7 +143,6 @@ GtsObjectClass ** gfs_classes (void)
       gfs_adapt_function_class (),
       gfs_adapt_gradient_class (),
         gfs_adapt_curvature_class (),
-      gfs_adapt_not_box_class (),
     gfs_event_sum_class (),
     gfs_event_harmonic_class (),
     gfs_event_stop_class (),
@@ -156,7 +157,8 @@ GtsObjectClass ** gfs_classes (void)
         gfs_source_viscosity_class (),
         gfs_source_friction_class (),
         gfs_source_coriolis_class (),
-        gfs_source_tension_class (),
+          gfs_source_tension_class (),
+          gfs_source_tension_css_class (),
 #if !FTT_2D
         gfs_source_hydrostatic_class (),
 #endif /* 2D3 or 3D */
@@ -257,103 +259,7 @@ void gfs_init (int * argc, char *** argv)
 		     (GLogFunc) gfs_log, NULL);
 
   /* Instantiates classes before reading any domain or simulation file */
-  gfs_global_class ();
-  gfs_simulation_class ();
-    gfs_ocean_class ();
-    gfs_advection_class ();
-    gfs_poisson_class ();
-
-  gfs_variable_class ();
-    gfs_variable_tracer_class ();
-    gfs_variable_residual_class ();
-    gfs_variable_filtered_class ();
-    gfs_variable_curvature_class ();
-
-  gfs_surface_bc_class ();
-
-  gfs_box_class ();
-    gfs_box_not_adapt_class ();
-  gfs_gedge_class ();
-
-  gfs_bc_dirichlet_class ();
-  gfs_bc_neumann_class ();
-  gfs_bc_flather_class ();
-
-  gfs_boundary_class ();
-    gfs_boundary_inflow_constant_class ();
-    gfs_boundary_outflow_class ();
-#ifdef HAVE_MPI
-    gfs_boundary_mpi_class ();
-#endif /* HAVE_MPI */
-
-  gfs_refine_class ();
-    gfs_refine_solid_class ();
-    gfs_refine_surface_class ();
-      gfs_refine_distance_class ();
-      gfs_refine_height_class ();
-
-  gfs_event_class ();
-    gfs_init_class ();
-    gfs_init_flow_constant_class ();
-    gfs_init_fraction_class ();
-#if FTT_2D
-    gfs_init_vorticity_class ();
-#endif /* FTT_2D */
-    gfs_adapt_class ();
-      gfs_adapt_vorticity_class ();
-      gfs_adapt_streamline_curvature_class ();
-      gfs_adapt_function_class ();
-      gfs_adapt_gradient_class ();
-        gfs_adapt_curvature_class ();
-      gfs_adapt_not_box_class ();
-    gfs_event_sum_class ();
-    gfs_event_harmonic_class ();
-    gfs_event_stop_class ();
-    gfs_event_script_class ();
-    gfs_source_generic_class ();
-      gfs_source_scalar_class ();
-        gfs_source_class ();
-          gfs_source_control_class ();
-        gfs_source_diffusion_class ();
-          gfs_source_diffusion_explicit_class ();
-      gfs_source_velocity_class ();
-        gfs_source_viscosity_class ();
-        gfs_source_friction_class ();
-        gfs_source_coriolis_class ();
-        gfs_source_tension_class ();
-#if !FTT_2D
-        gfs_source_hydrostatic_class ();
-#endif /* 2D3 or 3D */
-    gfs_remove_droplets_class ();
-    gfs_remove_ponds_class ();
-    gfs_event_filter_class ();
-   
-    gfs_output_class ();
-      gfs_output_time_class ();
-      gfs_output_progress_class ();
-      gfs_output_projection_stats_class ();
-      gfs_output_diffusion_stats_class ();
-      gfs_output_solid_stats_class ();
-      gfs_output_adapt_stats_class ();
-      gfs_output_timing_class ();
-      gfs_output_balance_class ();
-      gfs_output_solid_force_class ();
-      gfs_output_location_class ();
-      gfs_output_simulation_class ();
-      gfs_output_boundaries_class ();
-      gfs_output_particle_class ();
-
-      gfs_output_scalar_class ();
-        gfs_output_scalar_norm_class ();
-        gfs_output_scalar_stats_class ();
-        gfs_output_scalar_sum_class ();
-        gfs_output_scalar_maxima_class ();
-        gfs_output_scalar_histogram_class ();
-        gfs_output_error_norm_class ();
-          gfs_output_correlation_class ();
-	gfs_output_squares_class ();
-	gfs_output_streamline_class ();
-        gfs_output_ppm_class ();
+  gfs_classes ();
 
   /* If modules are not supported, calls modules init functions */
 #include "modules.c"
