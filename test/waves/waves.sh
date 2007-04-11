bf8f5bdc110d25cd345b130d0c08868eff0c019e
Stephane Popinet <popinet@users.sf.net>
2007-04-11 05:20:53 +0000

    New adaptive coastally-trapped wave test case

--- test/waves/waves.sh
+++ test/waves/waves.sh
@@ -2,7 +2,7 @@ if ! $donotrun; then
     rm -f correlation res-* sim-*
     shapes -n 500 ellipse | transform -s 1.999 -i > basin.gts
     for level in 5 6 7; do
-	if sed "s/LEVEL/$level/g" < $1 | gerris2D - | \
+	if sed "s/LEVEL/$level/g" < $1 | gerris2D3 - | \
 	    awk '{ print $1 " " $5 }' > res-$level && \
 	    awk -v l=$level 'BEGIN { min1 = 0. } {
          if ($2 > min1) {
@@ -18,7 +18,7 @@ if ! $donotrun; then
     done
 fi
 
-echo "Save solution.eps { format = EPS }" | gfsview-batch2D sim-7 solution.gfv
+echo "Save solution.eps { format = EPS }" | gfsview-batch2D3 sim-7 solution.gfv
 
 awk 'BEGIN {
   print "\\begin{tabular}{|c|c|c|}"
