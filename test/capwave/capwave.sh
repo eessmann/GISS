405faf5f21cf4dc90305ff9c04a662a4e0ecd3cf
Stephane Popinet <popinet@users.sf.net>
2007-04-20 05:35:50 +0000

    Updated wave tests to use implicit surfaces

--- test/capwave/capwave.sh
+++ test/capwave/capwave.sh
@@ -1,19 +1,6 @@
 levels="3 4 5 6"
 
 if ! $donotrun; then
-    if test -f wave.gts; then :
-    else
-	file=`mktemp /tmp/wave.XXXXXX`
-	awk 'BEGIN{
-          print "0.51 -0.51"
-          for (x = 0.51; x >= -0.51; x -= 0.01) 
-            print x " " 0.01*cos (2.*3.14159265359*x);
-          print "-0.51 -0.51"
-        }' > $file
-	shapes $file > wave.gts
-	rm -f $file
-    fi
-
     for level in $levels; do
 	if sed "s/LEVEL/$level/g" < $1 | gerris2D -; then
 	    :
