cfdbba7a1ce82490502275514700226206f68299
Stephane Popinet <s.popinet@niwa.co.nz>
2008-07-01 02:08:59 +0000

    Fix for doc generation with new gfs-highlight script

--- doc/examples/classes.c
+++ doc/examples/classes.c
@@ -4,17 +4,10 @@
 
 static void key_value_pair (const char * key, FILE * lang)
 {
-  static int first = 1;
-  if (first) {
-    fprintf (lang, "gfs_keyword = ");
-    first = 0;
-  }
-  else
-    fprintf (lang, ",\n");
-
+  fprintf (lang, "gfs_keyword = \"%s\"\n", key);
   /* keywords must start with Gfs */
   g_assert (strstr (key, "Gfs") == key);
-  fprintf (lang, "    '(Gfs){0,1}%s'", &(key[3]));
+  fprintf (lang, "gfs_keyword = \"%s\"\n", &(key[3]));
 }
 
 int main (int argc, char * argv[])
@@ -26,20 +19,23 @@ int main (int argc, char * argv[])
 
   printf ("# Language file for source-highlight\n"
 	  "# Generated automatically by classes.c\n"
-	  "\n"
+	  "\n");
+
+  key_value_pair ("GfsDefine", stdout);
+  key_value_pair ("GfsProjectionParams", stdout);
+  key_value_pair ("GfsApproxProjectionParams", stdout);
+
+  while (*klass) {
+    key_value_pair ((*klass)->info.name, stdout);
+    klass++;
+  }
+  
+  printf ("\n"
 	  "include \"cpp.lang\"\n"
 	  "\n"
 	  "comment start \"#\"\n"
 	  "\n"
 	  "redef preproc = \"C preprocessor command is not compatible with"
-	  " the use of # as comment character in GTS\"\n"
-	  "\n");
-  key_value_pair ("GfsDefine", stdout);
-  key_value_pair ("GfsProjectionParams", stdout);
-  key_value_pair ("GfsApproxProjectionParams", stdout);
-  while (*klass) {
-    key_value_pair ((*klass)->info.name, stdout);
-    klass++;
-  }
+	  " the use of # as comment character in GTS\"\n");
   return 0;
 }
