a11953d3a553d2598b47f8d87e525d778938e852
Stephane Popinet <popinet@users.sf.net>
2009-05-12 23:52:23 +0000

    Added 'launchpad-snapshot' Makefile target

--- debian/changelog.sh
+++ debian/changelog.sh
@@ -1,10 +1,25 @@
 GERRIS_VERSION=$(grep GFS_VERSION config.h | awk 'BEGIN{FS=" |\""}{print $4}')
 version=$(grep GFS_BUILD_VERSION src/version.h | awk 'BEGIN{FS=" |\"|-"}{print $4}')
 date=$(date +"%a, %e %b %Y %T %z")
-cat <<EOF > debian/changelog
-gerris-snapshot ($GERRIS_VERSION-$version) testing; urgency=low
+
+if test -z $1; then
+    cat <<EOF > debian/changelog
+gerris-snapshot ($GERRIS_VERSION-$version) hardy; urgency=low
 
   * gerris-snapshot release (based on Drew Parsons's official debian)
 
  -- Stephane Popinet <popinet@users.sf.net>  $date
 EOF
+else
+    cat <<EOF > debian/changelog
+gerris-snapshot ($GERRIS_VERSION-$version) hardy; urgency=low
+
+EOF
+    if html2text < $1 | grep -v "^[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} " >> debian/changelog; then :
+    else
+	exit 1
+    fi
+cat <<EOF >> debian/changelog
+ -- Stephane Popinet <popinet@users.sf.net>  $date
+EOF
+fi
