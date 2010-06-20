dce2b998c200e61b96820da4f925745a7e3a250f
Stephane Popinet <s.popinet@niwa.co.nz>
2010-06-20 10:14:55 +0000

    Added doc support for embedded videos

--- doc/examples/kaltura.sh
+++ doc/examples/kaltura.sh
@@ -1,7 +1,9 @@
 # insert the Kaltura.org javascript for HTML5 video
 # http://www.kaltura.org/project/HTML5_Video_Media_JavaScript_Library
 
-if grep -q "</video>" $1.html; then
-    sed 's/<\/HEAD>/<script type="text\/javascript" src="http:\/\/html5.kaltura.org\/js"><\/script>\n<\/HEAD>/' < $1.html > tmp
-    mv -f tmp $1.html
+f=`echo $1 | sed 's/\.html$//'`
+if grep -q "</video>" $f.html; then
+    tmp=`mktemp /tmp/kaltura.XXXXXXXXXX`
+    sed 's/<\/HEAD>/<script type="text\/javascript" src="http:\/\/html5.kaltura.org\/js"><\/script>\n<\/HEAD>/' < $f.html > $tmp
+    mv -f $tmp $f.html
 fi
