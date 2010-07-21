a7b31c1691762bb9cc8d026c844e22a02b1b44d6
Stephane Popinet <s.popinet@niwa.co.nz>
2010-07-21 22:56:55 +0000

    Keyword generation for externally installed modules

--- doc/examples/modulesyms.sh
+++ doc/examples/modulesyms.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-for f in $1/*.la; do
-    lib=`tr -d "'" < $f | grep dlname | cut -d= -f2-3`
-    nm -fb $1/.libs/$lib | grep ".* T gfs_.*_class$" | cut -d" " -f3-4
+for module in `cat modules.list`; do
+    for dim in 2D 3D; do
+	lib=$1/.libs/lib"$module""$dim".so
+	if test ! -f $lib; then
+	    lib=$2/lib"$module""$dim".so
+	fi
+	if test -f $lib; then
+	    nm -fb $lib | grep ".* T gfs_.*_class$" | grep -v "gfs_gl" | cut -d" " -f3-4
+	fi
+    done
 done | sort | uniq | sed -e 's/_class//g' -e 's/^./\U&/' -e 's/_./\U&/g' -e 's/_//g' | \
 awk '{ print $0; gsub ("^Gfs", ""); print $0; }'
