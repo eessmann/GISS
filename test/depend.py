b50811d74e297582f77d696aaa8a55be107e415f
Stephane Popinet <s.popinet@niwa.co.nz>
2011-01-20 20:13:10 +0000

    Support for parallel test suite

--- test/depend.py
+++ test/depend.py
@@ -9,7 +9,13 @@ import gfs2tex
 dists = ""
 depends = ""
 docs = ""
+tests = ""
 for start in sys.argv[1:]:
+    tests += "\\\n\t" + start + ".sh"
+    f = open(start + ".sh", "w")
+    f.write("python -u test.py " + start + "\n")
+    f.close()
+    os.chmod(start + ".sh",0755)
     for root, dirs, files in os.walk(start,topdown=True):
         if not ".xvpics" in root:
             test = gfs2tex.Example(root)
@@ -25,5 +31,7 @@ for start in sys.argv[1:]:
 print "DOCS = " + docs + dists
 print ""
 print "EXTRA_DIST += " + dists
+print ""
+print "TESTS = " + tests
 print ""
 print "tests.tex: " + depends
