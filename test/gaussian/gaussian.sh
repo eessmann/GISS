f9823ae7cfb25049e8b57a53c97c036ba2ac3a71
Stephane Popinet <s.popinet@niwa.co.nz>
2011-02-08 12:00:03 +0000

    Several fixes for the gaussian test case + new lonlat version

--- test/gaussian/gaussian.sh
+++ test/gaussian/gaussian.sh
@@ -1,6 +1,8 @@
+emax=$2
+
 if test x$donotrun != xtrue; then
     awk '{printf("%4.3f %4.3f 0.\n",0.,$1)}' < prof.ref > profile
-    if ( gerris2D -DLEVEL=7 -DSOLVER=$solver $1 ) ; then :
+    if ( gerris2D -DLEVEL=7 $1 ) ; then :
     else
 	exit 1
     fi     	
@@ -12,8 +14,13 @@ fi
 if cat <<EOF | python ; then :
 from check import *
 from sys import *
-if (Curve('prof.dat',3,5) - Curve('prof.ref',1,2)).max() > 2.5e-6:
-   print (Curve('prof.dat',3,5) - Curve('prof.ref',1,2)).max()
+avg = (Curve('prof.ref',1,2) - Curve('prof.dat',3,5)).mean()
+print avg
+c = Curve()
+for p in Curve('prof.dat',3,5).l:
+    c.l.append((p[0], p[1] + avg))
+if (Curve('prof.ref',1,2) - c).normi() > $emax:
+   print (Curve('prof.ref',1,2) - c).normi()
    exit(1)
 EOF
 else
