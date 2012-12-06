03e617d79d136500600f64700afe95a0528ec351
Stephane Popinet <s.popinet@gmail.com>
2012-12-06 04:38:21 +0000

    Do not use pipes within gnuplot for test cases
    Gnuplot seems to have problems when several instances are running in
    parallel.

--- test/lake/stratified/stratified.sh
+++ test/lake/stratified/stratified.sh
@@ -18,7 +18,9 @@ for nl in 8 16 32 64; do
     } END {
       print nl,emax
     }' < uprof-$nl
+    awk -f field.awk -v name="DRHO" end-$nl.txt | awk -f thermo.awk > thermo-$nl.txt
 done > error
+awk -f field.awk -v name="DRHO" end-64.txt > rho-64.txt
 
 if gnuplot <<EOF; then :
 set term postscript eps color lw 2 18 solid enhanced
@@ -46,10 +48,10 @@ plot 60./13.*1e-4*x+1 t 'analytical', \
 set output 'thermo.eps'
 set key top right
 plot -64./13.*1e-3*x+0.75 t 'analytical', \
-     '< awk -f field.awk -v name="DRHO" end-8.txt | awk -f thermo.awk' t '8 layers', \
-     '< awk -f field.awk -v name="DRHO" end-16.txt | awk -f thermo.awk' t '16 layers', \
-     '< awk -f field.awk -v name="DRHO" end-32.txt | awk -f thermo.awk' t '32 layers', \
-     '< awk -f field.awk -v name="DRHO" end-64.txt | awk -f thermo.awk' t '64 layers'
+     'thermo-8.txt' t '8 layers', \
+     'thermo-16.txt' t '16 layers', \
+     'thermo-32.txt' t '32 layers', \
+     'thermo-64.txt' t '64 layers'
 
 set output 'error.eps'
 fm(x)=am+bm*x
@@ -74,7 +76,7 @@ unset key
 set palette defined ( 0 0 0 0.5647, 0.125 0 0.05882 1, 0.25 0 0.5647 1,\
      0.375 0.05882 1 0.9333, 0.5 0.5647 1 0.4392, 0.625 1 0.9333 0, 0.75 1 0.4392 0,\
      0.875 0.9333 0 0, 1 0.498 0 0 )
-splot [-5:5][0:1.01]'< awk -v name="DRHO" -f field.awk end-64.txt' u 1:2:3
+splot [-5:5][0:1.01]'rho-64.txt' u 1:2:3
 EOF
 else
     exit 1
