66918c837baffe83676f6e737eb5f085a6c3d12f
Erich Essmann <erich@hv-kb9-cluster.is.ed.ac.uk>
2016-09-27 15:35:59 +0100

    Docker Config

--- Docker/GISS_PATH.sh
+++ Docker/GISS_PATH.sh
@@ -16,3 +16,26 @@ export HYPRE=/Deployment/hypre
 export PATH=$PATH:$HYPRE/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HYPRE/lib
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HYPRE/lib/pkgconfig
+
+
+cd /GISS/Third_Party/ode
+bash bootstrap
+bash configure --enable-shared=yes --enable-double-precision --enable-builtin-threading-impl --enable-libccd --prefix=/Deployment/ODE
+make -j6
+make install
+
+cd /GISS/Third_Party/hypre/src
+bash configure --prefix=$HYPRE --enable-shared
+make -j6 
+make install
+
+cd /GISS/Third_Party/gts
+bash configure --prefix=/Deployment/GISS
+make -j6
+make install
+
+cd /GISS/Third_Party/gerris_Inuse
+/sbin/ldconfig -v
+CPPFLAGS=-I$HYPRE/include LDFLAGS="-L$HYPRE/lib" ./configure --prefix=$GISS
+make -j6
+make install
